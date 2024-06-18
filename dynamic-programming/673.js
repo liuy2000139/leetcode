066576a2684d5f7b7e2e6781ce108ef4d8e9a314
Liam.YA.Liu <liam.ya.liu@cn.pwc.com>
2024-06-18 09:55:54 +0800

    dp

--- dynamic-programming/673.js
+++ dynamic-programming/673.js
@@ -4,24 +4,31 @@
  */
 var findNumberOfLIS = function (nums) {
   let dp = []
-  let res = []
-  for (let i = nums.length - 1; i >= 0; i--) {
-    dp[i] = []
-    for (let j = i; j < nums.length; j++) {
-      if (i === j) {
-        dp[i][j] = 1
-      } else {
-        if (nums[j] > nums[j - 1] && dp[i][j - 1] === dp[i + 1][j]) {
-          dp[i][j] = dp[i][j - 1] + 1
-        } else {
-          dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j])
+  let cnt = []
+  let m = nums.length
+  let res = 0
+  let maxLen = 0
+  for (let i = 0; i < m; i++) {
+    dp[i] = 1
+    cnt[i] = 1
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        if (dp[j] + 1 > dp[i]) {
+          dp[i] = dp[j] + 1
+          cnt[i] = cnt[j]
+        } else if (dp[j] + 1 === dp[i]) {
+          cnt[i] += cnt[j]
         }
       }
-      res[dp[i][j]] = res[dp[i][j]] || 0 + 1
+    }
+
+    if (dp[i] > maxLen) {
+      maxLen = dp[i]
+      res = cnt[i] // 重置计数
+    } else if (dp[i] === maxLen) {
+      res += cnt[i]
     }
   }
-
   return res
 }
-// console.log(findNumberOfLIS([2, 2, 2, 2, 2]))
 console.log(findNumberOfLIS([1, 3, 5, 4, 7]))
